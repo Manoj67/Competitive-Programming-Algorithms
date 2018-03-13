3dca503c81af08bd570e379b9b5436ed3d3cc09a
Manoj67 <fastestmk330@gmail.com>
2018-03-13 08:43:57 +0530

    adding value 51

--- experiment.cpp
+++ experiment.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main(){
+    int x = 51;
     cout << "nothing is bad" << endl;
     cout << "what" << endl
-    cout << "that is different" << endl;    
+    cout << "that is different" << endl;
 }
